e459fb66576978172eb8724ab8385e338d2d75f8
LelyH <lely2011@gmail.com>
2022-10-19 19:03:12 -0700

    added index.js, index.html, README

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# professional-README-generator
-Professional README Generator
+# Professional README Generator
+
+## Description
+
+- The motivation to this generator is create a professional style of a README for projects.
+- This project is built to help create a professional and readable README file for all coding projects
+- This solves users to remember all components needed in a README file by adding description, tablet of contents, installation, the use of application, credits, and license.
+- I learned all of the components to create an acceptable README for future users to reference what the application does
+
+## Installation
+
+A started code was used to better organize the code sequence:
+
+Link: https://github.com/coding-boot-camp/potential-enigma 
+
+## Usage
+
+Provide instructions and examples for use. Include screenshots as needed.
+
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+    ```md
+    ![alt text](assets/images/screenshot.png)
+    ```
+
+## Credits
+
+Starter Code from: https://github.com/coding-boot-camp/potential-enigma 
+README guide sample and explaination: https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide
+
+## License
+
+MIT No Attribution
+
+Copyright 2022 Lely Huynh
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ index.js
@@ -0,0 +1,97 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const generateREADME = ({ name, description, tableContents, installation, usage, credits, license }) =>
+`# ${name}
+
+## ${description}
+
+Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+
+- What was your motivation?
+- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
+- What problem does it solve?
+- What did you learn?
+
+## ${tableContents}
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## ${installation}
+
+What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+
+## ${usage}
+
+Provide instructions and examples for use. Include screenshots as needed.
+
+## ${credits}
+
+List your collaborators, if any, with links to their GitHub profiles.
+
+If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+
+If you followed tutorials, include links to those here as well.
+
+## ${license}
+
+The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+
+---`
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your project name?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is your project about?',
+      },
+      {
+        type: 'input',
+        name: 'tableContents',
+        message: 'Does your project have multiple sections?',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Are there additional installation needed?',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Are there instruction and example for use?',
+      },
+      {
+        type: 'input',
+        name: 'credits',
+        message: 'Any credits to add?',
+      },
+      {
+        type: 'input',
+        name: 'license',
+        message: 'Which license would you like to include?',
+      },
+  ])
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
